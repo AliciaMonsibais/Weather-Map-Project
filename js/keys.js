59ae423671401706c925e2d26062625e97a65838
AliciaMonsibais <51135985+AliciaMonsibais@users.noreply.github.com>
2019-08-16 14:42:03 -0500

    Delete keys.js

--- js/keys.js
+++
@@ -1,3 +0,0 @@
-var mapboxToken = "pk.eyJ1IjoiYWxpY2lhbW9uc2liYWlzIiwiYSI6ImNqeGF2ZXlmejAzMmMzem82YnBjNHB1N2oifQ.HRAIKtSnMrU2tZgcrJAX5g";
-var darkSkyKey = "9dcf55d61e85cfaa3f6d848a2fed0f62";
-var gitHubKey = "9529d2d47ed3a89ea0480a4f14b80739241ec814";
